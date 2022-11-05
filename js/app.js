2a1816fcb9f18ef7f8a73daba7ea59492a5a8df3
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-11-05 11:46:11 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,126 @@
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content')
+
+function pageTransitions() {
+    // button click active class
+
+    for (let i = 0; i < secBtn.length; i++) {
+        secBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        });
+    }
+
+    // sections active class
+
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+
+        if (id) {
+            // remove selected from other bts
+            secBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            });
+            e.target.classList.add('active');
+
+            // hide other sections
+            
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+
+    // toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',  () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+}
+
+pageTransitions();
+
+// type writter
+
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid crimson}";
+        document.body.appendChild(css);
+    };
+
+
+
+
+var $form = $('form#contact-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwD-V0gIyPLjlfNquMg7wyUFvF6ijSogLG5sUoG2epwDuucop-yJLYHllMGACSiQ7Y7BQ/exec'
+
+$('#submit').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
